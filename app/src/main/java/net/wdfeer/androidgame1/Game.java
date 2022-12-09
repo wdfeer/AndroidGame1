09c97677b1dead46bb3c991ea7eeef6b6f97279c
wdfeer <49950781+wdfeer@users.noreply.github.com>
2022-12-09 11:04:46 +0100

    Lock max framerate

--- app/src/main/java/net/wdfeer/androidgame1/Game.java
+++ app/src/main/java/net/wdfeer/androidgame1/Game.java
@@ -11,22 +11,18 @@ public class Game {
     }
     public static void restart(){
         gameObjects = new ArrayList<>();
-        frameCount = 0;
         for (int i = 0; i < 100; i++) {
             Body body = new Body(new V2f((float) (Math.random() * 1000), (float) (Math.random() * 1000)), 50, Color.rgb(i % 3 / 3f, i % 4 / 4f, i % 6 / 6f));
             body.velocity = new V2f((float)Math.random() - 0.5f, (float)Math.random() - 0.5f);
+            body.velocity = body.velocity.mult(10f);
             gameObjects.add(body);
         }
     }
     public static ArrayList<GameObject> gameObjects;
-    public static int frameCount;
     public static void update(){
-        frameCount++;
         for (GameObject gameObject : gameObjects) {
             gameObject.update();
         }
-        if (frameCount >= 600)
-            restart();
     }
     public static void draw(Canvas canvas){
         canvas.drawColor(Color.BLACK);
--- app/src/main/java/net/wdfeer/androidgame1/MainThread.java
+++ app/src/main/java/net/wdfeer/androidgame1/MainThread.java
@@ -5,13 +5,21 @@ import android.view.SurfaceHolder;
 
 public class MainThread extends Thread {
 
+
     public MainThread(SurfaceHolder surfaceHolder, GameView gameView) {
         super();
         this.surfaceHolder = surfaceHolder;
         this.gameView = gameView;
         Game.initialize();
     }
-
+    public static final int targetFPS = 60;
+    long startTime;
+    long timeMillis;
+    long waitTime;
+    long totalTime = 0;
+    int frameCount = 0;
+    long targetTime = 1000 / targetFPS;
+    float averageFPS;
     private final SurfaceHolder surfaceHolder;
     private final GameView gameView;
     public boolean running;
@@ -19,6 +27,7 @@ public class MainThread extends Thread {
     @Override
     public void run() {
         while (running) {
+            startTime = System.nanoTime();
             canvas = null;
 
             try {
@@ -27,14 +36,31 @@ public class MainThread extends Thread {
                     Game.update();
                     Game.draw(canvas);
                 }
-            } catch (Exception ignored) {} finally {
-                if (canvas != null) {
-                    try {
-                        surfaceHolder.unlockCanvasAndPost(canvas);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+            } catch (Exception ignored) {}
+            finally {
+                    if (canvas != null)            {
+                        try {
+                            surfaceHolder.unlockCanvasAndPost(canvas);
+                        }
+                    catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
+            timeMillis = (System.nanoTime() - startTime) / 1000000;
+            waitTime = targetTime - timeMillis;
+
+            try {
+                sleep(waitTime);
+            } catch (Exception ignored) {}
+
+            totalTime += System.nanoTime() - startTime;
+            frameCount++;
+            if (frameCount == targetFPS)        {
+                averageFPS = 1000f / ((totalTime / frameCount) / 1000000f);
+                frameCount = 0;
+                totalTime = 0;
+                System.out.println("Average FPS: " + averageFPS);
             }
         }
     }
