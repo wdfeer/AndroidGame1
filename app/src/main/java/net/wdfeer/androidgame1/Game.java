de171be69a30b0c634df07b3ba5f5998442ddc88
wdfeer <49950781+wdfeer@users.noreply.github.com>
2022-12-09 10:19:58 +0100

    Add restarting the game

--- app/src/main/java/net/wdfeer/androidgame1/Game.java
+++ app/src/main/java/net/wdfeer/androidgame1/Game.java
@@ -7,17 +7,26 @@ import java.util.ArrayList;
 
 public class Game {
     public static void initialize(){
+        restart();
+    }
+    public static void restart(){
+        gameObjects = new ArrayList<>();
+        frameCount = 0;
         for (int i = 0; i < 100; i++) {
             Body body = new Body(new V2f((float) (Math.random() * 1000), (float) (Math.random() * 1000)), 50, Color.rgb(i % 3 / 3f, i % 4 / 4f, i % 6 / 6f));
             body.velocity = new V2f((float)Math.random() - 0.5f, (float)Math.random() - 0.5f);
             gameObjects.add(body);
         }
     }
-    public static ArrayList<GameObject> gameObjects = new ArrayList<>();
+    public static ArrayList<GameObject> gameObjects;
+    public static int frameCount;
     public static void update(){
+        frameCount++;
         for (GameObject gameObject : gameObjects) {
             gameObject.update();
         }
+        if (frameCount >= 600)
+            restart();
     }
     public static void draw(Canvas canvas){
         canvas.drawColor(Color.BLACK);
